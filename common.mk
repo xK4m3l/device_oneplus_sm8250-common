cb4c4428cc3dc5caac52d667ed97e6870037af07
xK4m3l <lp77684@gmail.com>
2021-12-21 17:06:21 +0100

    sm8250-common: Strek bringup

--- common.mk
+++ common.mk
@@ -46,7 +46,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-streak
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
